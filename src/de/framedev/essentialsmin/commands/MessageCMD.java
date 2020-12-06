c54cb58ca849293331c681a9d228b7db53bd12c5
TraxMC <darrylhuber39@gmail.com>
2020-12-06 16:11:52 +0100

    MessagesCMD and TeleportCMD new Features added!

--- src/de/framedev/essentialsmin/commands/MessageCMD.java
+++ src/de/framedev/essentialsmin/commands/MessageCMD.java
@@ -25,16 +25,38 @@ public class MessageCMD implements CommandExecutor {
 
     public MessageCMD(Main plugin) {
         this.plugin = plugin;
-        plugin.getCommands().put("msg",this);
-        plugin.getCommands().put("r",this);
-        plugin.getCommands().put("spy",this);
+        plugin.getCommands().put("msg", this);
+        plugin.getCommands().put("r", this);
+        plugin.getCommands().put("spy", this);
+        plugin.getCommands().put("msgtoggle",this);
     }
 
     HashMap<Player, Player> reply = new HashMap<>();
     ArrayList<Player> spy = new ArrayList<>();
+    ArrayList<Player> msgToggle = new ArrayList<>();
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("msgtoggle")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (player.hasPermission(plugin.getPermissionName() + "msgtoggle")) {
+                    if (msgToggle.contains(player)) {
+                        msgToggle.remove(player);
+                        player.sendMessage(plugin.getPrefix() + "§aMessages will be sent to you!");
+                        return true;
+                    } else {
+                        player.sendMessage(plugin.getPrefix() + "§cNo more messages will be sent to you!");
+                        msgToggle.add(player);
+                        return true;
+                    }
+                } else {
+                    player.sendMessage(plugin.getPrefix() + plugin.getNOPERMS());
+                }
+            } else {
+                sender.sendMessage(plugin.getPrefix() + plugin.getOnlyPlayer());
+            }
+        }
         if (command.getName().equalsIgnoreCase("msg")) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
@@ -42,21 +64,44 @@ public class MessageCMD implements CommandExecutor {
                 if (player.hasPermission("essentialsmini.msg")) {
                     if (args.length >= 2) {
                         Player target = Bukkit.getPlayer(args[0]);
-                        if (target != null) {
-                            for (int i = 1; i < args.length; i++) {
-                                message = message + args[i] + " ";
-                            }
-                            for(Player opPlayer : Bukkit.getOnlinePlayers()) {
-                                if(opPlayer.hasPermission("essentialsmini.spy")) {
-                                    if(spy.contains(opPlayer)) {
-                                        opPlayer.sendMessage("§6" + player.getName() + " §ahat eine Nachricht an §6" + target.getName() + " §agesendet mit dem Text §6: §c" + message);
+                        if (!msgToggle.contains(target)) {
+                            if (target != null) {
+                                for (int i = 1; i < args.length; i++) {
+                                    message = message + args[i] + " ";
+                                }
+                                for (Player opPlayer : Bukkit.getOnlinePlayers()) {
+                                    if (opPlayer.hasPermission("essentialsmini.spy")) {
+                                        if (spy.contains(opPlayer)) {
+                                            opPlayer.sendMessage("§6" + player.getName() + " §ahat eine Nachricht an §6" + target.getName() + " §agesendet mit dem Text §6: §c" + message);
+                                        }
                                     }
                                 }
+                                player.sendMessage("§cme §r-> §a " + target.getName() + " §f» " + message);
+                                target.sendMessage("§a" + player.getName() + " §r-> §cme  §f» " + message);
+                                message = "";
+                                reply.put(target, player);
                             }
-                            player.sendMessage("§cme §r-> §a " + target.getName() + " §f» " + message);
-                            target.sendMessage("§a" + player.getName() + " §r-> §cme  §f» " + message);
-                            message = "";
-                            reply.put(target, player);
+                        } else if(player.hasPermission(plugin.getPermissionName() + "msgtoggle.bypass")) {
+                            if (target != null) {
+                                for (int i = 1; i < args.length; i++) {
+                                    message = message + args[i] + " ";
+                                }
+                                for (Player opPlayer : Bukkit.getOnlinePlayers()) {
+                                    if (opPlayer.hasPermission("essentialsmini.spy")) {
+                                        if (spy.contains(opPlayer)) {
+                                            opPlayer.sendMessage("§6" + player.getName() + " §ahat eine Nachricht an §6" + target.getName() + " §agesendet mit dem Text §6: §c" + message);
+                                        }
+                                    }
+                                }
+                                player.sendMessage("§cme §r-> §a " + target.getName() + " §f» " + message);
+                                target.sendMessage("§a" + player.getName() + " §r-> §cme  §f» " + message);
+                                message = "";
+                                reply.put(target, player);
+                            }
+                        } else {
+                            if (target != null) {
+                                player.sendMessage(plugin.getPrefix() + "§cThis Player doesn't Allow Messages!");
+                            }
                         }
                     } else {
                         player.sendMessage(plugin.getPrefix() + plugin.getWrongArgs("/msg <PlayerName> <Message>"));
@@ -81,9 +126,9 @@ public class MessageCMD implements CommandExecutor {
                                     for (int i = 0; i < args.length; i++) {
                                         message = message + args[i] + " ";
                                     }
-                                    for(Player opPlayer : Bukkit.getOnlinePlayers()) {
-                                        if(opPlayer.hasPermission("essentialsmini.spy")) {
-                                            if(spy.contains(opPlayer)) {
+                                    for (Player opPlayer : Bukkit.getOnlinePlayers()) {
+                                        if (opPlayer.hasPermission("essentialsmini.spy")) {
+                                            if (spy.contains(opPlayer)) {
                                                 opPlayer.sendMessage("§6" + player.getName() + " §ahat eine Nachricht an §6" + target.getName() + " §agesendet mit dem Text §6: §c" + message);
                                             }
                                         }
@@ -94,7 +139,7 @@ public class MessageCMD implements CommandExecutor {
                                     reply.remove(player);
                                 }
                             } else {
-                                player.sendMessage(plugin.getPrefix()+ "§aDir wurde vor kurzem keine Nachricht geschrieben!");
+                                player.sendMessage(plugin.getPrefix() + "§aDir wurde vor kurzem keine Nachricht geschrieben!");
                             }
                         }
                     } else {
@@ -107,8 +152,8 @@ public class MessageCMD implements CommandExecutor {
                 sender.sendMessage(plugin.getPrefix() + plugin.getOnlyPlayer());
             }
         }
-        if(command.getName().equalsIgnoreCase("spy")) {
-            if(sender instanceof Player) {
+        if (command.getName().equalsIgnoreCase("spy")) {
+            if (sender instanceof Player) {
                 Player player = (Player) sender;
                 if (player.hasPermission("essentialsmini.spy")) {
                     if (!spy.contains(player)) {
