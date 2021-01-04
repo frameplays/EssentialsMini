d2b7ea7090f82962057d430fb1fcd15b3cf1e82f
frame-dev <darrylhuber39@gmail.com>
2021-01-04 23:25:35 +0100

    Test Commit

--- src/de/framedev/essentialsmin/commands/PayCMD.java
+++ src/de/framedev/essentialsmin/commands/PayCMD.java
@@ -33,8 +33,6 @@ public class PayCMD extends CommandBase {
         setupTabCompleter("pay", this);
     }
 
-    /* Nope */
-
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("pay")) {
