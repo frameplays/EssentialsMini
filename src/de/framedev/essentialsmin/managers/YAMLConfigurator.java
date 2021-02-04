f297ab746b4ea7e083edbfe86900f0c4d45c988f
frame-dev <darrylhuber39@gmail.com>
2021-02-04 17:33:00 +0100

    Yaml Config Added

---
+++ src/de/framedev/essentialsmin/managers/YAMLConfigurator.java
@@ -0,0 +1,102 @@
+package de.framedev.essentialsmin.managers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.MapType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import de.framedev.essentialsmin.main.Main;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * This Plugin was Created by FrameDev
+ * Package : de.framedev.essentialsmin.managers
+ * Date: 04.02.21
+ * Project: EssentialsMini
+ * Copyrighted by FrameDev
+ */
+
+public class YAMLConfigurator {
+
+    private HashMap<String,Object> data = getConfig();
+
+    private HashMap<String, Object> getConfig() {
+        File file = new File(Main.getInstance().getDataFolder(),"conf.yml");
+        ObjectMapper mapper = new YAMLMapper();
+        HashMap<String, Object> hash = new HashMap<>();
+        try {
+            MapType type = TypeFactory.defaultInstance().constructMapType(HashMap.class, String.class, Object.class);
+            hash = mapper.readValue(new FileReader(file),type);
+        } catch (Exception ignored) {
+        }
+        return hash;
+    }
+
+    public void set(String path, Object value) {
+        data.put(path,value);
+    }
+
+    public int getInt(String path) {
+        if(data.containsKey(path))
+            return Integer.parseInt(String.valueOf(data.get(path)));
+        return 0;
+    }
+
+    public double getDouble(String path) {
+        if(data.containsKey(path))
+            return Double.parseDouble(String.valueOf(data.get(path)));
+        return 0d;
+    }
+
+    public Object get(String path) {
+        if(data.containsKey(path))
+            return data.get(path);
+        return null;
+    }
+
+    public String getString(String path) {
+        if(data.containsKey(path))
+            return (String) data.get(path);
+        return null;
+    }
+
+    public boolean isInteger(String path) {
+        if(data.containsKey(path)) {
+            try {
+                Integer.parseInt(String.valueOf(data.get(path)));
+                return true;
+            } catch (Exception ignored) {
+
+            }
+        }
+        return false;
+    }
+
+    public boolean isDouble(String path) {
+        if(data.containsKey(path)) {
+            try {
+                Double.parseDouble(String.valueOf(data.get(path)));
+                return true;
+            } catch (Exception ignored) {}
+        }
+        return false;
+    }
+
+    public boolean contains(String path) {
+        return data.containsKey(path);
+    }
+
+    public void saveConfig() {
+        File file = new File(Main.getInstance().getDataFolder(),"conf.yml");
+        ObjectMapper mapper = new YAMLMapper();
+        try {
+            mapper.writeValue(new FileWriter(file),data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
