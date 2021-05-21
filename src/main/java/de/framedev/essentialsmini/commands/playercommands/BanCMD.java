2083e8ae65df07a66e8442753f6fa284607c4d6b
frame-dev <darrylhuber39@gmail.com>
2021-05-21 23:57:14 +0200

    BanCMD Updated

--- src/main/java/de/framedev/essentialsmini/commands/playercommands/BanCMD.java
+++ src/main/java/de/framedev/essentialsmini/commands/playercommands/BanCMD.java
@@ -9,24 +9,25 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import java.util.*;
+
 public class BanCMD extends CommandBase {
 
 	public BanCMD(Main plugin) {
-		super(plugin, "eban");
+		super(plugin);
+		setup("eban", this);
+		setupTabCompleter("eban", this);
 		setup(this);
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(sender.hasPermission("essentialsmini.ban")) {
-			if(args.length >= 1) {
-				StringBuilder message = new StringBuilder();
-				for (int i = 1; i < args.length; i++) {
-					message.append(args[i]).append(" ");
-				}
-				BanFile.banPlayer(args[0], message.toString());
+			if(args.length == 2) {
+				BanType type = BanType.valueOf(args[1].toUpperCase());
+				BanFile.banPlayer(args[0], type.getReason());
 				if (Bukkit.getPlayer(args[0]) != null) {
-					Bukkit.getPlayer(args[0]).kickPlayer(ChatColor.RED + "You are Banned while " + ChatColor.GOLD + BanFile.cfg.getString(args[0] + ".reason"));
+					Bukkit.getPlayer(args[0]).kickPlayer(ChatColor.RED + "You are Banned while " + type.getReason());
 				} else {
 					sender.sendMessage(getPlugin().getPrefix() + getPlugin().getVariables().getPlayerNotOnline());
 				}
@@ -35,5 +36,37 @@ public class BanCMD extends CommandBase {
 			}
 		}
 		return false;
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+		if(args.length == 2) {
+			ArrayList<String> types = new ArrayList<>();
+			Arrays.asList(BanType.values()).forEach(type -> types.add(type.name()));
+			ArrayList<String> empty = new ArrayList<>();
+			for(String s : types) {
+				if (s.toLowerCase().startsWith(args[1].toLowerCase())) {
+					empty.add(s);
+				}
+			}
+			Collections.sort(empty);
+			return empty;
+		}
+		return super.onTabComplete(sender, command, label, args);
+	}
+
+	public static enum BanType {
+		HACKING("hacking"),
+		TRY_BYPASS_BAN("try bypassing a Ban");
+
+		private final String reason;
+
+		BanType(String reason) {
+			this.reason = reason;
+		}
+
+		public String getReason() {
+			return reason;
+		}
 	}
 }
