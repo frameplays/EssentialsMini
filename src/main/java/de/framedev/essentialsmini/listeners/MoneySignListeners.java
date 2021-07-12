4c655bfa7e929fd9139713c9c8ae2a502aaf2a5d
frame-dev <darrylhuber39@gmail.com>
2021-07-12 15:55:46 +0200

    MoneySignListeners Updated / Performance Fix

--- src/main/java/de/framedev/essentialsmini/listeners/MoneySignListeners.java
+++ src/main/java/de/framedev/essentialsmini/listeners/MoneySignListeners.java
@@ -31,7 +31,7 @@ public class MoneySignListeners implements Listener {
     }
 
     @EventHandler
-    public void onclickSignBalance(SignChangeEvent e) {
+    public void onSignChangeBalance(SignChangeEvent e) {
         if (e.getLine(0).equalsIgnoreCase("[balance]")) {
             if (e.getPlayer().hasPermission("essentialsmini.signs.create")) {
                 String signName = Main.getInstance().getConfig().getString("MoneySign.Balance");
@@ -57,7 +57,7 @@ public class MoneySignListeners implements Listener {
                 if (s.getLine(0).equalsIgnoreCase(signName)) {
                     if (e.getPlayer().hasPermission("essentialsmini.signs.use")) {
                         String money = String.valueOf(eco.getBalance(e.getPlayer()));
-                        String text = Main.getInstance().getConfig().getString("Money.MSG.Balance");
+                        String text = Main.getInstance().getCustomMessagesConfig().getString("Money.MSG.Balance");
                         text = text.replace("[Money]", money);
                         text = text.replace('&', '§');
                         e.getPlayer().sendMessage(text);
@@ -65,6 +65,71 @@ public class MoneySignListeners implements Listener {
                         e.getPlayer().sendMessage(Main.getInstance().getPrefix() + Main.getInstance().getNOPERMS());
                     }
                 }
+                String signNameFree = Main.getInstance().getConfig().getString("MoneySign.Free");
+                signNameFree = signNameFree.replace('&', '§');
+                if (s.getLine(0).equalsIgnoreCase(signNameFree)) {
+                    if (e.getPlayer().hasPermission("essentialsmini.signs.use")) {
+                        String[] args = s.getLines();
+                        Material name = Material.getMaterial(args[1].toUpperCase());
+                        int amount = Integer.parseInt(args[2]);
+                        if (s.getLine(1).equalsIgnoreCase(name.name()) && s.getLine(2).equalsIgnoreCase(amount + "")) {
+                            e.getPlayer().getInventory().addItem(new ItemStack(name, amount));
+                        }
+                    } else {
+                        e.getPlayer().sendMessage(Main.getInstance().getPrefix() + Main.getInstance().getNOPERMS());
+                    }
+                }
+                String signNameBuy = Main.getInstance().getConfig().getString("MoneySign.Buy");
+                signNameBuy = signNameBuy.replace('&', '§');
+                if (s.getLine(0).equalsIgnoreCase(signNameBuy)) {
+                    if (e.getPlayer().hasPermission("essentialsmini.signs.use")) {
+                        String[] args = s.getLines();
+                        Material name = Material.getMaterial(args[1].toUpperCase());
+
+                        int amount = Integer.parseInt(args[2]);
+
+                        int money = Integer.parseInt(args[3].replace(Main.getInstance().getCurrencySymbolMulti(), ""));
+                        if (s.getLine(1).equalsIgnoreCase(name.name()) && s.getLine(2).equalsIgnoreCase(amount + "") && s.getLine(3).equalsIgnoreCase(money + "" + Main.getInstance().getCurrencySymbolMulti())) {
+                            if (eco.getBalance(e.getPlayer()) < money) {
+                                e.getPlayer().sendMessage("Not enought Money");
+                                return;
+                            }
+                            ItemStack item = new ItemStack(name);
+                            item.setAmount(amount);
+                            e.getPlayer().getInventory().addItem(item);
+                            eco.withdrawPlayer(e.getPlayer(), money);
+                            e.getPlayer().sendMessage("§aYou bought §6" + name.name() + " §afor §6" + money + Main.getInstance().getCurrencySymbolMulti());
+                        }
+                    } else {
+                        e.getPlayer().sendMessage(Main.getInstance().getPrefix() + Main.getInstance().getNOPERMS());
+                    }
+                }
+                String signNameSell = Main.getInstance().getConfig().getString("MoneySign.Sell");
+                signNameSell = signNameSell.replace('&', '§');
+                if (s.getLine(0).equalsIgnoreCase(signNameSell))
+                    if (e.getPlayer().hasPermission("essentialsmini.signs.use")) {
+                        String[] args = s.getLines();
+                        Material name = Material.getMaterial(args[1].toUpperCase());
+
+                        int amount = Integer.parseInt(args[2]);
+
+                        int money = Integer.parseInt(args[3].replace(Main.getInstance().getCurrencySymbolMulti(), ""));
+                        if (s.getLine(1).equalsIgnoreCase(name.name())
+                                && s.getLine(2).equalsIgnoreCase(amount + "")
+                                && s.getLine(3).equalsIgnoreCase(money + "" + Main.getInstance().getCurrencySymbolMulti())) {
+                            if (e.getPlayer().getInventory().contains(name, amount)) {
+                                ItemStack item = new ItemStack(name);
+                                item.setAmount(amount);
+                                e.getPlayer().getInventory().removeItem(item);
+                                eco.depositPlayer(e.getPlayer(), money);
+                                e.getPlayer().sendMessage("§aYou sell §6" + name.name() + " §afor §6" + money + Main.getInstance().getCurrencySymbolMulti());
+                                return;
+                            }
+                            e.getPlayer().sendMessage("Not enought " + name.name());
+                        }
+                    } else {
+                        e.getPlayer().sendMessage(Main.getInstance().getPrefix() + Main.getInstance().getNOPERMS());
+                    }
             }
         }
     }
@@ -116,74 +181,6 @@ public class MoneySignListeners implements Listener {
     }
 
     @EventHandler
-    public void onInteractFree(PlayerInteractEvent e) {
-        if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (e.getHand() == EquipmentSlot.OFF_HAND) {
-                return;
-            }
-            if (e.getHand().equals(EquipmentSlot.HAND)) {
-                String signName = Main.getInstance().getConfig().getString("MoneySign.Free");
-                signName = signName.replace('&', '§');
-                if (e.getClickedBlock().getState() instanceof Sign) {
-                    Sign s = (Sign) e.getClickedBlock().getState();
-                    if (s.getLine(0).equalsIgnoreCase(signName)) {
-                        if (e.getPlayer().hasPermission("essentialsmini.signs.use")) {
-                            String[] args = s.getLines();
-                            Material name = Material.getMaterial(args[1].toUpperCase());
-                            int amount = Integer.parseInt(args[2]);
-                            if (s.getLine(1).equalsIgnoreCase(name.name()) && s.getLine(2).equalsIgnoreCase(amount + "")) {
-                                e.getPlayer().getInventory().addItem(new ItemStack(name, amount));
-                            }
-                        } else {
-                            e.getPlayer().sendMessage(Main.getInstance().getPrefix() + Main.getInstance().getNOPERMS());
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-
-    @EventHandler
-    public void onInteract(PlayerInteractEvent e) {
-        if (e.getAction() == Action.RIGHT_CLICK_BLOCK &&
-                e.getClickedBlock().getState() instanceof Sign) {
-            if (e.getHand() == EquipmentSlot.OFF_HAND) {
-                return;
-            }
-            if (e.getHand().equals(EquipmentSlot.HAND)) {
-                String signName = Main.getInstance().getConfig().getString("MoneySign.Buy");
-                signName = signName.replace('&', '§');
-                Sign s = (Sign) e.getClickedBlock().getState();
-                if (s.getLine(0).equalsIgnoreCase(signName)) {
-                    if (e.getPlayer().hasPermission("essentialsmini.signs.use")) {
-                        String[] args = s.getLines();
-                        Material name = Material.getMaterial(args[1].toUpperCase());
-
-                        int amount = Integer.parseInt(args[2]);
-
-                        int money = Integer.parseInt(args[3].replace(Main.getInstance().getCurrencySymbolMulti(), ""));
-                        if (s.getLine(1).equalsIgnoreCase(name.name()) && s.getLine(2).equalsIgnoreCase(amount + "") && s.getLine(3).equalsIgnoreCase(money + "" + Main.getInstance().getCurrencySymbolMulti())) {
-                            if (eco.getBalance(e.getPlayer()) < money) {
-                                e.getPlayer().sendMessage("Not enought Money");
-                                return;
-                            }
-                            ItemStack item = new ItemStack(name);
-                            item.setAmount(amount);
-                            e.getPlayer().getInventory().addItem(item);
-                            eco.withdrawPlayer(e.getPlayer(), money);
-                            e.getPlayer().sendMessage("§aYou bought §6" + name.name() + " §afor §6" + money);
-                        }
-                    } else {
-                        e.getPlayer().sendMessage(Main.getInstance().getPrefix() + Main.getInstance().getNOPERMS());
-                    }
-                }
-            }
-        }
-    }
-
-
-    @EventHandler
     public void signChangeSell(SignChangeEvent e) {
         String signName = Main.getInstance().getConfig().getString("MoneySign.Sell");
         signName = signName.replace('&', '§');
@@ -207,42 +204,4 @@ public class MoneySignListeners implements Listener {
             }
         }
     }
-
-    @EventHandler
-    public void onInteractSell(PlayerInteractEvent e) {
-        if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (e.getHand() == EquipmentSlot.OFF_HAND) {
-                return;
-            }
-            if (e.getHand().equals(EquipmentSlot.HAND) &&
-                    e.getClickedBlock().getState() instanceof Sign) {
-                String signName = Main.getInstance().getConfig().getString("MoneySign.Sell");
-                signName = signName.replace('&', '§');
-                Sign s = (Sign) e.getClickedBlock().getState();
-                if (s.getLine(0).equalsIgnoreCase(signName))
-                    if (e.getPlayer().hasPermission("essentialsmini.signs.use")) {
-                        String[] args = s.getLines();
-                        Material name = Material.getMaterial(args[1].toUpperCase());
-
-                        int amount = Integer.parseInt(args[2]);
-
-                        int money = Integer.parseInt(args[3].replace(Main.getInstance().getCurrencySymbolMulti(), ""));
-                        if (s.getLine(1).equalsIgnoreCase(name.name()) && s.getLine(2).equalsIgnoreCase(amount + "") && s.getLine(3).equalsIgnoreCase(money + "" + Main.getInstance().getCurrencySymbolMulti())) {
-                            if (e.getPlayer().getInventory().contains(name, amount)) {
-                                ItemStack item = new ItemStack(name);
-                                item.setAmount(amount);
-                                e.getPlayer().getInventory().removeItem(item);
-                                eco.depositPlayer(e.getPlayer(), money);
-                                e.getPlayer().sendMessage("§aYou sell §6" + name.name() + " §afor §6" + money);
-                                return;
-                            }
-                            e.getPlayer().sendMessage("Not enought " + name.name());
-                        }
-                    } else {
-                        e.getPlayer().sendMessage(Main.getInstance().getPrefix() + Main.getInstance().getNOPERMS());
-                    }
-            }
-        }
-    }
-
 }
